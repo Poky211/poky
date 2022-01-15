13c01d555332a3ca5231bc8e24ad5907bc7e6885
Poky211 <97640379+Poky211@users.noreply.github.com>
2022-01-14 23:24:35 -0300

    Add files via upload

--- assets/preload/data/bopeebo/script.lua
+++ assets/preload/data/bopeebo/script.lua
@@ -1,26 +1,9 @@
-function start(song) -- do nothing
-
-end
-
-function update(elapsed)
-
-end
-
-function beatHit(beat) -- do nothing
-
-end
-
-function stepHit(step) -- do nothing
-
-    if(step == 40) then
-        noteTweenX()
-    end
-end
-
-function playerTwoTurn()
-    tweenCameraZoom(1.3,(crochet * 4) / 1000)
-end
-
-function playerOneTurn()
-    tweenCameraZoom(1,(crochet * 4) / 1000)
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		startVideo('Cutscene1Subtitles');
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
 end
